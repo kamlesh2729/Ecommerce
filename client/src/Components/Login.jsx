c8d98be1fd9cfed32022efd9c8e27eb5633406ff
Kamlesh Dilip Tankar <kamleshtankar96@gmail.com>
2024-10-31 21:16:08 +0530

    Auth done

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,34 +1,44 @@
 import React, { useState } from "react";
 import { useDispatch} from "react-redux";
 import { useNavigate } from "react-router";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 import { login } from "../actions/authaction";
 
 const Login = ({ handelSwitch}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const[visability, setVisability] = useState(false);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handelSubmit = (e) => {
     e.preventDefault();
-    if (!email || !password) {
-      alert("Enter email and password");
+    if (!email) {
+      setMessage("Please enter email")
+    }
+    if (!password) {
+      setMessage("Please enter password")
     }
     dispatch(login({ email, password }, navigate));
   };
 
+  const PasswordShowHide = () => {
+    setVisability(!visability);
+  }
+
   return (
-    <section className=" w-[80vw] h-[70vh] flex flex-col justify-center items-center mx-auto login-bg relative z-3">
-      <h1 className=" uppercase font-semibold text-h4 mx-auto absolute z-3 top-0">
+    <section className=" w-[80vw] h-[80vh] flex flex-col justify-center items-center mx-auto py-8 login-bg relative z-3">
+      <h1 className=" uppercase font-semibold text-h4 mx-auto mt-4 absolute z-3 top-0">
         Log In here
       </h1>
       <form
         className="w-3/4 h-[50vh] flex flex-col items-center absolute z-3"
         onSubmit={handelSubmit}
       >
-        <div className="w-full Lp-l:w-3/4 flex flex-col Lp-l:flex-row items-center justify-between gap-4">
+        <div className="w-full Lp-l:w-3/4 relative flex flex-col items-start justify-between gap-4">
           <label htmlFor="Email" className="text-p font-semibold mb-4">
             Email<b className=" text-blue-500">*</b>
           </label>
@@ -41,25 +51,33 @@ const Login = ({ handelSwitch}) => {
               setEmail(e.target.value);
             }}
           />
+          <p className=" absolute bottom-2 text-xl">{message}</p>
         </div>
 
-        <div className="w-full Lp-l:w-3/4 flex flex-col Lp-l:flex-row items-center justify-between gap-4">
+        <div className="w-full Lp-l:w-3/4 relative flex flex-col items-start justify-between gap-4">
           <label htmlFor="msg" className="text-p font-semibold mb-4">
             Passaword<b className=" text-blue-500">*</b>
           </label>
           <input
             className="w-full bg-slate-200 py-5 px-7 text-p border outline-none mb-9 focus:border-b-2 focus:border-t-0 border-r-0 border-l-0 focus:border-text-orange"
-            type="password"
-            name="password"
+            type={visability ? "text" : "password"}
             id="Password"
             onChange={(e) => {
               setPassword(e.target.value);
             }}
           />
+          <span className=" absolute right-12 top-24 text-bt cursor-pointer">
+            {visability ? (
+              <FaEyeSlash onClick={PasswordShowHide} />
+            ) : (
+              <FaEye onClick={PasswordShowHide} />
+            )}
+          </span>
+          <p className=" absolute bottom-2 text-xl">{message}</p>
         </div>
 
         <button
-          className="w-full Lp-l:w-3/6 bg-blue-500 flex justify-center items-center py-7 px-12 rounded-md transition-all border border-solid border-white text-white text-bt font-semibold cursor-pointer hover:shadow-3xr hover:bg-text-orange"
+          className="w-full Lp-l:w-3/6 flex justify-center items-center py-7 px-12 mt-4 rounded-md transition-all border border-solid border-black text-black text-bt font-semibold cursor-pointer hover:shadow-3xr hover:bg-black hover:text-white"
           type="submit"
         >
           Log In
--- client/src/Components/Signup.jsx
+++ client/src/Components/Signup.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useRef, useState} from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 import { signup } from "../actions/authaction";
 
@@ -8,28 +9,62 @@ const Signup = ({ handelSwitch }) => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [showpassword, setShowPassword] = useState("password");
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const passwordref = useRef(null);
 
   const handelSubmit = (e) => {
     e.preventDefault();
-    if (!email || !password || !name) {
-      alert("Enter firstname and lastname and email and password");
+    const lower = new RegExp("(?=.*[a-z])");
+    const upper = new RegExp("(?=.*[A-Z])");
+    const number = new RegExp("(?=.*[0-9])");
+    const spcialchar = new RegExp("(?=.*[!@#$%^&*])");
+    const length = new RegExp("(?=.(6,)])");
+
+    if (!name) {
+    setMessage("Plaese enter name");
     }
+
+    if (email === '') {
+    setMessage("Plaese enter email");
+    }
+
+    if (!password) {
+      setMessage("Plaese enter password");
+    }else if (!lower.test(password)) {
+      setMessage("At least one lower case latter include");
+    } else if (!upper.test(password)) {
+      setMessage("At least one upper case latter include");
+    } else if (!number.test(password)) {
+      setMessage("At least one number include");
+    } else if (!spcialchar.test(password)) {
+      setMessage("At least one spcial character include");
+    } else if (!length.test(password)) {
+      setMessage("Password lenght must be min 6");
+    } else {
     dispatch(signup({ name, email, password }, navigate));
+    }
+
+    // dispatch(signup({ name, email, password }, navigate));
+  };
+
+  const PasswordShowHide = () => {
+    setShowPassword(!showpassword);
   };
 
   return (
-    <section className="w-[80vw] h-[90vh] flex flex-col justify-center items-center mx-auto signup-bg">
-      <h1 className=" uppercase font-semibold text-h4 mx-auto absolute z-3 top-0">
+    <section className="w-[80vw] h-[100vh] flex flex-col justify-center items-center mx-auto py-4 signup-bg">
+      <h1 className=" uppercase font-semibold text-h4 mx-auto mt-4 absolute z-3 top-0">
         Sign Up here
       </h1>
       <form
         className="w-3/4 flex flex-col items-center absolute z-3 "
         onSubmit={handelSubmit}
       >
-        <div className="w-full Lp-l:w-3/4 flex flex-col Lp-l:flex-row items-center justify-between gap-4">
+        <div className="w-full Lp-l:w-3/4 relative flex flex-col items-start justify-between gap-4">
           <label htmlFor="Fname" className="text-p font-semibold mb-4">
             First Name <b className=" text-blue-500">*</b>
           </label>
@@ -42,9 +77,10 @@ const Signup = ({ handelSwitch }) => {
               setName(e.target.value);
             }}
           />
+          <p className=" absolute bottom-2 text-xl">{message}</p>
         </div>
 
-        <div className="w-full Lp-l:w-3/4 flex flex-col Lp-l:flex-row items-center justify-between gap-4">
+        <div className="w-full Lp-l:w-3/4 relative flex flex-col items-start justify-between gap-4">
           <label htmlFor="Email" className="text-p font-semibold mb-4">
             Email<b className=" text-blue-500">*</b>
           </label>
@@ -56,26 +92,37 @@ const Signup = ({ handelSwitch }) => {
             onChange={(e) => {
               setEmail(e.target.value);
             }}
+            required
           />
+          <p className=" absolute bottom-2 text-xl">{message}</p>
         </div>
 
-        <div className="w-full Lp-l:w-3/4 flex flex-col Lp-l:flex-row items-center justify-between gap-4">
+        <div className="w-full Lp-l:w-3/4 relative flex flex-col items-start justify-between gap-4">
           <label htmlFor="Password" className="text-p font-semibold mb-4">
             Passaword<b className=" text-blue-500">*</b>
           </label>
           <input
             className="w-full bg-slate-200 py-5 px-7 text-p border outline-none mb-9 focus:border-b-2 focus:border-t-0 border-r-0 border-l-0 focus:border-text-orange"
-            type="password"
+            type={showpassword ? "text" : "password"}
             name="password"
             id="Password"
+            ref={passwordref}
             onChange={(e) => {
               setPassword(e.target.value);
             }}
           />
+          <span className=" absolute right-12 top-24 text-bt cursor-pointer">
+            {showpassword ? (
+              <FaEye onClick={PasswordShowHide} />
+            ) : (
+              <FaEyeSlash onClick={PasswordShowHide} />
+            )}
+          </span>
+          <p className=" absolute bottom-2 text-xl">{message}</p>
         </div>
 
         <button
-          className="w-full Lp-l:w-3/4 bg-blue-500 flex justify-center items-center py-7 px-12 rounded-md transition-all border border-solid border-text-orange text-white text-bt font-semibold cursor-pointer hover:shadow-3xr hover:bg-text-orange"
+          className="w-full Lp-l:w-3/6 flex justify-center items-center py-7 px-12 mt-4 rounded-md text-black text-bt font-semibold border-[1px] border-black cursor-pointer hover:shadow-3xr hover:bg-black hover:text-white"
           type="submit"
         >
           Sign Up
